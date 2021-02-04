25bf7468122744bf5cabc526d78b9e9e019e5d60
Vitalii Kravchyshyn <vitalii.kravchyshyn@paybee.io>
2021-02-05 01:34:59 +0200

    create simple server, which handle get and post events

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
-// console.log('Hello world', __dirname, __filename);
-
-// __dirname - вказує на директорію в якій ми ведемо розробку
-// Users/vitaliikravchyshyn/own-projects/nodejs-basics
-// __filename - вказує на файл в якому ми ведемо розробку
-// Users/vitaliikravchyshyn/own-projects/nodejs-basics/index.js
-
-// const obj = require('./user');
-
-// console.log(obj);
-
-// console.log(obj.sayHello());
-
-
-// Використовуєтьсядля створення та налаштування власного сервера
+// Використовується для створення та налаштування власного сервера
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log('starting');
-    console.log(req.url);
+    if (req.method === 'GET') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
 
-    res.write('<h1>Hello from NodeJS</h1>');
-    res.write('<h2>Hello from NodeJS</h2>');
-    res.write('<h3>Hello from NodeJS</h3>');
-    res.end(`
-        <div style="background: orange; width:200px; height:200px">
-            <h2>Test 1</h2>
-        </div>
-    `);
+        res.end(`
+            <h1>Form</h1>
+            <form method="post" action="/">
+                <input name="title" type="text" />
+                <button type="submit">Snd</button>
+            </form>
+        `)
+    } else if (req.method === 'POST') {
+        const body = [];
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        })
+
+        req.on('data', (data) => {
+            body.push(Buffer.from(data));
+        });
+
+        req.on('end', () => {
+            const message = body.toString().split('=')[1];
+            res.end(`
+            <h1>Ваше повідомлення з форми: ${message}</h1>
+        `)
+        });
+    }
 });
 
 server.listen(5000, () => {
