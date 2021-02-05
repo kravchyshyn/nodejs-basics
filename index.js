369d57ca995466da4857f70843f0e8b292792379
Vitalii Kravchyshyn <vitalii.kravchyshyn@paybee.io>
2021-02-05 02:07:07 +0200

    create API server, add simple API handling

--- index.js
+++ index.js
@@ -8,21 +8,38 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, {
             'Content-Type': 'text/html; charset=utf-8'
         });
-        
+
         if (req.url === '/') {
-            fs.readFile(path.join(__dirname, 'views', 'index.html'), (err, content) => {
-                if (err) throw err;
+            fs.readFile(
+                path.join(__dirname, 'views', 'index.html'), 
+                'utf-8',
+                (err, content) => {
+                    if (err) throw err;
 
-                res.end(content)
-            })
+                    res.end(content)
+                })
         } else if (req.url === '/about') {
-            fs.readFile(path.join(__dirname, 'views', 'about.html'), (err, content) => {
-                if (err) throw err;
+            fs.readFile(
+                path.join(__dirname, 'views', 'about.html'), 
+                'utf-8',
+                (err, content) => {
+                    if (err) throw err;
 
-                res.end(content)
-            });
+                    res.end(content)
+                });
+        } else if (req.url === '/api/users') {
+            res.writeHead(200, {
+                'Content-Type': 'text/json'
+            })
+
+            const users = [
+                {name: 'Vitalik', age: 29},
+                {name: 'Nastya', age: 23}
+            ];
+
+            res.end(JSON.stringify(users));
+        
         }
-
     } else if (req.method === 'POST') {
         const body = [];
         res.writeHead(200, {
